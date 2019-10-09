1e9f960bf52e0cbeab91b0b90b79e4f2e0d1d6bc
samtufail <miansamusama@gmail.com>
2019-10-09 23:47:33 +0500

    changes

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(___dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(___dirname, "client", "build", "index.html"));
   });
 }
 
