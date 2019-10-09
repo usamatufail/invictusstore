5b124452e57ae21274d59c285673f21917ca7ea4
samtufail <miansamusama@gmail.com>
2019-10-09 21:47:11 +0500

    Production Build Added

---
+++ client/build/precache-manifest.997a0613e1f83ffa05239da609ff2b42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39a4d0afccac7e23760487a9504707ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75404e6a8817cd564577",
+    "url": "/static/css/2.684c4774.chunk.css"
+  },
+  {
+    "revision": "89dad82ddb3301c5fff9",
+    "url": "/static/css/main.0ac9667b.chunk.css"
+  },
+  {
+    "revision": "75404e6a8817cd564577",
+    "url": "/static/js/2.357acbd8.chunk.js"
+  },
+  {
+    "revision": "89dad82ddb3301c5fff9",
+    "url": "/static/js/main.5a98f14b.chunk.js"
+  },
+  {
+    "revision": "64e5f7f277cee90ef727",
+    "url": "/static/js/runtime~main.c4ffe073.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "8d19e39f4fa1768d5ebfd322d2955434",
+    "url": "/static/media/invictuslogo.8d19e39f.png"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "da6654f0b302f73ea0e632f38d1a3300",
+    "url": "/static/media/landing-bg.da6654f0.png"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-
+const connectDB = require("./config/db");
+const path = require("path");
 //Connect Database
 connectDB();
 
 //Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/product', require('./routes/api/products'));
-app.use('/api/categories', require('./routes/api/categories'));
-app.use('/api/orders', require('./routes/api/orders'));
-app.use('/api/cart', require('./routes/api/cart'));
-app.use('/api/contact', require('./routes/api/contact'));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/product", require("./routes/api/products"));
+app.use("/api/categories", require("./routes/api/categories"));
+app.use("/api/orders", require("./routes/api/orders"));
+app.use("/api/cart", require("./routes/api/cart"));
+app.use("/api/contact", require("./routes/api/contact"));
 
-app.use(express.static('uploads'));
-
+app.use(express.static("./uploads"));
+app.use(express.static("./"));
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(___dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
 
 // M1075
 // mongodb://localhost:27017/invitcus-eCom
